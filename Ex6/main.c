9ad038f0bd068bd47679c1063693df4a8244e050
mariuler <mariuler>
2017-10-03 12:08:19 +0200

    Ex6 done
    Signed-off-by: mariuler <mariuler>

--- Ex6/main.c
+++ Ex6/main.c
@@ -25,7 +25,6 @@ void* disturbance(void* arg){
 
 void print_periodic(void* arg){
 	rt_task_set_periodic(NULL, TM_NOW, 500000*1000);
-	rt_printf("I print once");
 	while(1){
 		rt_printf("Test\n");
 		rt_task_wait_period(NULL);
@@ -34,13 +33,11 @@ void print_periodic(void* arg){
 }
 
 void task(void* arg){
-	int* ptr = (int*) arg;
-	int channel = *ptr;
+	int channel = (int)arg;
+	rt_printf("Channel %d\n", channel);
 	io_write(channel,1);
-	rt_printf("I print once");
+	rt_task_set_periodic(NULL, TM_NOW, 10*1000);
 	while(1){
-		
-		
 		if (!io_read(channel)){
 				io_write(channel, 0);
 				usleep(5);
@@ -52,24 +49,23 @@ void task(void* arg){
 	return;
 }
 
-
-
 int main(int argc, char const *argv[]){	
 	io_init();
 	rt_print_auto_init(1);
 	mlockall(MCL_CURRENT | MCL_FUTURE);
 
+	RT_TASK tasks[3];
 	int channelA = 1;
-	//int channelB = 2;
-	//int channelC = 3;
+	int channelB = 2;
+	int channelC = 3;
 
-	RT_TASK taskA;
-
-	rt_task_create(&taskA, "TaskA", 0, 50, T_CPU(3));
-	rt_task_start(&taskA, &print_periodic, NULL);
+	rt_task_create(&tasks[0], NULL, 0, 50, T_CPU(3));
+	rt_task_create(&tasks[1], NULL, 0, 50, T_CPU(3));
+	rt_task_create(&tasks[2], NULL, 0, 50, T_CPU(3));
+	rt_task_start(&tasks[0], &task, (void*)channelA);
+	rt_task_start(&tasks[1], &task, (void*)channelB);
+	rt_task_start(&tasks[2], &task, (void*)channelC);
 	
-
-
 	pthread_t disturbancethreads[10];
 	int i;
 	for (i = 0; i < 10; ++i)
@@ -77,5 +73,6 @@ int main(int argc, char const *argv[]){
 		pthread_create(&disturbancethreads[i],NULL,disturbance,NULL);
 	}
 	pthread_join(disturbancethreads[5],NULL);
+	
 	return 0;
 }
