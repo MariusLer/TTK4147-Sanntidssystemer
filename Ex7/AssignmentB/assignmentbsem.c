02f6dbf894d5eae1543b4c23c13cdcc87260fde0
mariusler <mariuler@stud.ntnu.no>
2017-10-10 12:09:53 +0200

    Did ex7

--- Ex7/AssignmentB/assignmentbsem.c
+++ Ex7/AssignmentB/assignmentbsem.c
@@ -8,8 +8,6 @@
 #include <native/mutex.h>
 
 
-RT_SEM semaphore;
-
 int rt_task_sleep_ms(unsigned long delay){
 	return rt_task_sleep(1000*1000*delay);
 }
@@ -33,41 +31,45 @@ void print_pri(RT_TASK *task, char *s){
 
 void L(void* arg){
 	RT_TASK* self = rt_task_self();
-	RT_MUTEX* mutex = (RT_MUTEX*)arg;
+	RT_SEM* sem = (RT_SEM*)arg;
 	print_pri(self,"Task L Waiting \n");
-	rt_sem_p(&semaphore,TM_INFINITE);
+	rt_sem_p(sem,TM_INFINITE);
 
-
-	 rt_mutex_acquire(mutex,TM_INFINITE);
+	while(1){
+	 rt_sem_p(sem,TM_INFINITE);
 	 busy_wait_ms(3);
 	 print_pri(self,"Task L signal sem \n");
-	 rt_mutex_release(mutex);
+	 rt_sem_v(sem);
+	}
 }
 
 void M(void* arg){
 	RT_TASK* self = rt_task_self();
+	RT_SEM* sem = (RT_SEM*)arg;
 	print_pri(self,"Task M Waiting \n");
-	rt_sem_p(&semaphore,TM_INFINITE);
+	rt_sem_p(sem,TM_INFINITE);
 
 
-
+	while(1){
 		rt_task_sleep_ms(1);
 		busy_wait_ms(5);
 		print_pri(self,"Task M did work \n");
+	}
 
 }
 void H(void* arg){
 	RT_TASK* self = rt_task_self();
-	RT_MUTEX* mutex = (RT_MUTEX*)arg;
+	RT_SEM* sem = (RT_SEM*)arg;
 	print_pri(self,"Task H Waiting \n");
-	rt_sem_p(&semaphore,TM_INFINITE);
+	rt_sem_p(sem,TM_INFINITE);
 
-	
+	while(1){
 		rt_task_sleep_ms(2);
-		rt_mutex_acquire(mutex,TM_INFINITE);
+		rt_sem_p(sem,TM_INFINITE);
 		busy_wait_ms(2);
 		print_pri(self,"Task H signal sem \n");
-		rt_mutex_release(mutex);
+		rt_sem_v(sem);
+	}
 
 }
 
@@ -85,24 +87,22 @@ int main()
 	rt_print_auto_init(1);
 	mlockall(MCL_CURRENT | MCL_FUTURE);
 
-	RT_MUTEX mutex;
+	RT_SEM semaphore;
 	RT_TASK tasks[4];
-	rt_mutex_create(&mutex, NULL);
-	rt_sem_create(&semaphore, NULL,0, S_PRIO);
+	rt_sem_create(&semaphore, NULL, 0, S_PRIO);
 
 	rt_task_create(&tasks[0], NULL, 0, 5, T_CPU(3) | T_JOINABLE);
 	rt_task_create(&tasks[1], NULL, 0, 10, T_CPU(3) | T_JOINABLE);
 	rt_task_create(&tasks[2], NULL, 0, 15, T_CPU(3) | T_JOINABLE);
 	rt_task_create(&tasks[3], NULL, 0, 20, T_CPU(3) | T_JOINABLE);
-	rt_task_start(&tasks[0], &L, (void*)&mutex);
-	rt_task_start(&tasks[1], &M, (void*)&mutex);
-	rt_task_start(&tasks[2], &H, (void*)&mutex);
+	rt_task_start(&tasks[0], &L, (void*)&semaphore);
+	rt_task_start(&tasks[1], &M, (void*)&semaphore);
+	rt_task_start(&tasks[2], &H, (void*)&semaphore);
 	rt_task_start(&tasks[3], &synchronize, (void*)&semaphore);
 
 	rt_task_join(&tasks[0]);
 	rt_task_join(&tasks[1]);
 	rt_task_join(&tasks[2]);
 	rt_sem_delete(&semaphore);
-	rt_mutex_delete(&mutex); 
 	return 0;
 }
--- Ex7/AssignmentCD/main.c
+++ Ex7/AssignmentCD/main.c
@@ -22,7 +22,6 @@ void busy_wait_ms(unsigned long delay){
 	}
 }
 
-
 void print_pri(RT_TASK *task, char *s){
 	struct rt_task_info temp;
 	rt_task_inquire(task, &temp);
@@ -33,41 +32,52 @@ void print_pri(RT_TASK *task, char *s){
 
 void L(void* arg){
 	RT_TASK* self = rt_task_self();
-	RT_MUTEX* mutex = (RT_MUTEX*)arg;
+	RT_MUTEX* mutexes;
+	mutexes = (RT_MUTEX*)arg;
+	RT_MUTEX mutexA = mutexes[0];
+	RT_MUTEX mutexB = mutexes[1];
+
 	print_pri(self,"Task L Waiting \n");
 	rt_sem_p(&semaphore,TM_INFINITE);
 
-
-	 rt_mutex_acquire(mutex,TM_INFINITE);
+	while(1){
+	 rt_mutex_acquire(&mutexA,TM_INFINITE);
+	 rt_task_set_priority(self,15);
 	 busy_wait_ms(3);
-	 print_pri(self,"Task L signal sem \n");
-	 rt_mutex_release(mutex);
+	 print_pri(self,"Task L got mutexA \n");
+	 rt_mutex_acquire(&mutexB, TM_INFINITE);
+	 print_pri(self,"Task L got mutexB \n");
+	 busy_wait_ms(3);
+	 rt_mutex_release(&mutexB);
+	 rt_mutex_release(&mutexA);
+	 rt_task_set_priority(self,5);
+	 rt_task_sleep_ms(1);
+	}
 }
 
-void M(void* arg){
-	RT_TASK* self = rt_task_self();
-	print_pri(self,"Task M Waiting \n");
-	rt_sem_p(&semaphore,TM_INFINITE);
-
-
-
-		rt_task_sleep_ms(1);
-		busy_wait_ms(5);
-		print_pri(self,"Task M did work \n");
-
-}
 void H(void* arg){
 	RT_TASK* self = rt_task_self();
-	RT_MUTEX* mutex = (RT_MUTEX*)arg;
+	RT_MUTEX* mutexes;
+	mutexes = (RT_MUTEX*)arg;
+	RT_MUTEX mutexA = mutexes[0];
+	RT_MUTEX mutexB = mutexes[1];
+
+
 	print_pri(self,"Task H Waiting \n");
 	rt_sem_p(&semaphore,TM_INFINITE);
 
-	
-		rt_task_sleep_ms(2);
-		rt_mutex_acquire(mutex,TM_INFINITE);
-		busy_wait_ms(2);
-		print_pri(self,"Task H signal sem \n");
-		rt_mutex_release(mutex);
+	while(1){
+		rt_task_sleep_ms(1);
+		rt_mutex_acquire(&mutexB,TM_INFINITE);
+	 	busy_wait_ms(1);
+	 	print_pri(self,"Task H got mutexB \n");
+	 	rt_mutex_acquire(&mutexA, TM_INFINITE);
+	  	print_pri(self,"Task H got mutexA \n");
+	 	busy_wait_ms(2);
+	 	rt_mutex_release(&mutexA);
+	 	rt_mutex_release(&mutexB);
+	 	busy_wait_ms(1);
+	}
 
 }
 
@@ -85,24 +95,26 @@ int main()
 	rt_print_auto_init(1);
 	mlockall(MCL_CURRENT | MCL_FUTURE);
 
-	RT_MUTEX mutex;
-	RT_TASK tasks[4];
-	rt_mutex_create(&mutex, NULL);
+	RT_MUTEX mutex[2];
+	RT_TASK tasks[3];
+	rt_mutex_create(&mutex[0], NULL);
+	rt_mutex_create(&mutex[1], NULL);
 	rt_sem_create(&semaphore, NULL,0, S_PRIO);
 
 	rt_task_create(&tasks[0], NULL, 0, 5, T_CPU(3) | T_JOINABLE);
 	rt_task_create(&tasks[1], NULL, 0, 10, T_CPU(3) | T_JOINABLE);
 	rt_task_create(&tasks[2], NULL, 0, 15, T_CPU(3) | T_JOINABLE);
-	rt_task_create(&tasks[3], NULL, 0, 20, T_CPU(3) | T_JOINABLE);
-	rt_task_start(&tasks[0], &L, (void*)&mutex);
-	rt_task_start(&tasks[1], &M, (void*)&mutex);
-	rt_task_start(&tasks[2], &H, (void*)&mutex);
-	rt_task_start(&tasks[3], &synchronize, (void*)&semaphore);
+	rt_task_start(&tasks[0], &L, (void*)mutex);
+	rt_task_start(&tasks[1], &H, (void*)mutex);
+	rt_task_start(&tasks[2], &synchronize, (void*)&semaphore);
 
 	rt_task_join(&tasks[0]);
 	rt_task_join(&tasks[1]);
 	rt_task_join(&tasks[2]);
 	rt_sem_delete(&semaphore);
-	rt_mutex_delete(&mutex); 
+	int i;
+	for (i = 0; i < 2; i++){
+		rt_mutex_delete(&mutex[i]);
+	}
 	return 0;
 }
